5453e7b834caeebaea29e257002553cabb6bb020
Richi409 <richard.matatsch@gmail.com>
2024-12-14 15:32:40 +0100

    linux/.config/02_nvim/lua/richi_409/lazy/telescope.lua - added additional dependency to make searches faster

--- linux/.config/02_nvim/lua/richi_409/lazy/telescope.lua
+++ linux/.config/02_nvim/lua/richi_409/lazy/telescope.lua
@@ -2,11 +2,24 @@ return {
     "nvim-telescope/telescope.nvim",
 
 	dependencies = {
-		"nvim-lua/plenary.nvim"
+		"nvim-lua/plenary.nvim",
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
 	},
 
     config = function()
-        require('telescope').setup({})
+        -- require('telescope').setup({})
+        require('telescope').setup {
+            --[[ pickers = {
+                find_files = {
+                    theme = "ivy"
+                }
+            }, ]]--
+            extensions = {
+                fzf = {}
+            }
+        }
+
+        require('telescope').load_extension('fzf')
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
