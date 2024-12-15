5c49a8c29050c7667677b99f32d2f5d1d9ceded2
Richi409 <richard.matatsch@gmail.com>
2024-12-15 16:08:55 +0100

    linux/.config/02_nvim - updated "nvim" configuration   - split configuration in new files to make "init.lua" cleaner   - added configuration for the builtin terminal in vim

---
+++ linux/.config/02_nvim/lua/richi_409/terminal_config.lua
@@ -0,0 +1,26 @@
+vim.api.nvim_create_autocmd('TermOpen', {
+    desc = 'disable line numbers when opening a terminal in vim',
+    group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+    callback = function ()
+        vim.opt.number = false
+        vim.opt.relativenumber = false
+    end
+})
+
+-- local job_id = 0
+vim.keymap.set("n", "<space>st", function ()
+    vim.cmd.vnew()
+    vim.cmd.term()
+    vim.cmd.wincmd("J")
+    vim.api.nvim_win_set_height(0, 10)
+
+    -- job_id = vim.bo.channel
+end)
+
+vim.keymap.set("t", "<space><ESC>", "<C-\\><C-n>")
+
+--[[
+vim.keymap.set("n", "<space>example", function ()
+    vim.fn.chansend(job_id, {"echo 'hi'\r\n"})
+end)
+]]--
