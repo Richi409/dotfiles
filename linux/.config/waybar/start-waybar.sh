f335e3db6d9c6e3ec1617852f92c04d43a6682fc
Richi409 <richard.matatsch@gmail.com>
2026-01-16 23:37:22 +0100

    added waybar configuration for niri

--- linux/.config/waybar/start-waybar.sh
+++ linux/.config/waybar/start-waybar.sh
@@ -12,6 +12,14 @@ if pgrep -x "waybar" > /dev/null; then
     done
 fi
 
+if [ "${DESKTOP_SESSION}" = "niri" ]; then
+    waybar -b 0 -c ~/.config/waybar/niri_config.jsonc -s ~/.config/waybar/style.css >/dev/null 2>&1 &
+elif [ "${DESKTOP_SESSION}" = "sway" ]; then
+    waybar -b 0 -c ~/.config/waybar/sway_config.jsonc -s ~/.config/waybar/style.css >/dev/null 2>&1 &
+else
+    waybar -b 0 -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css >/dev/null 2>&1 &
+fi
+
 # Launch waybar
-waybar -b 0 -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css >/dev/null 2>&1 &
+# waybar -b 0 -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css >/dev/null 2>&1 &
 # waybar -b 0 -c ~/.config/waybar/bars/sway/config.jsonc -s ~/.config/waybar/bars/sway/style.css >/dev/null 2>&1 &
